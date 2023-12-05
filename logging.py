fec41e9a7bde4ca324ad2e5c570339fe62bb1bfb
Edm1795 <aswitzer@ualberta.ca>
2023-12-05 14:41:51 -0700

    Create logging.py

---
+++ logging.py
@@ -0,0 +1,14 @@
+import logging
+
+logging.basicConfig(format='%(asctime)s - %(message)s', datefmt='%b-%d-%y %H:%M:%S') # only keep this for testing
+# basicConfig(format='%(process)d-%(levelname)s-%(message)s') # Use this to include the level of the message
+
+# Configure the logger (only call this once at outset of program, further calls will have no effect; filemode default is 'a' (append)
+logging.basicConfig(level=logging.DEBUG,filename='practice.log', filemode='w', format='%(asctime)s - %(message)s', datefmt='%d-%b-%y %H:%M:%S')
+
+logging.debug('This will get logged')
+logging.debug('This is a debug message')
+logging.info('This is an info message')
+logging.warning('This is a warning message')
+logging.error('This is an error message')
+logging.critical('This is a critical message')
